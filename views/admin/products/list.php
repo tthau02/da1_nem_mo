6276d711f35bd8a3e95739eaa6c719f8b1889990
Qk29 <qkbn2001@gmail.com>
2024-11-18 12:30:21 +0700

    sap xep lai cot

--- views/admin/products/list.php
+++ views/admin/products/list.php
@@ -27,11 +27,13 @@
                             <th scope="col" class="text-center align-middle"></th>
                             <th scope="col" class="text-center align-middle">#ID</th>
                             <th scope="col" class="text-center align-middle">Name</th>
+                            <th scope="col" class="text-center align-middle">Category</th>
                             <th scope="col" class="text-center align-middle">Image</th>
                             <th scope="col" class="text-center align-middle">Price</th>
                             <th scope="col" class="text-center align-middle">Quantity</th>
                             <th scope="col" class="text-center align-middle">Status</th>
-                            <th scope="col" class="text-center align-middle">Description</th>
+                            
+                            <th scope="col" class="text-center align-middle">Thao Tác</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -44,13 +46,15 @@
                                 </td>
                                 <td class="text-center align-middle"><?= $pro['id'] ?></td>
                                 <td class="text-center align-middle"><?= $pro['name'] ?></td>
+                                <td class="text-center align-middle"><?= $pro['cate_name'] ?></td>
                                 <td class="text-center align-middle">
                                     <img src="<?= ROOT_URL . $pro['image'] ?>" width="60" alt="">
                                 </td>
                                 <td class="text-center align-middle"><?= $pro['price'] ?></td>
                                 <td class="text-center align-middle"><?= $pro['quantity'] ?></td>
+                                
                                 <td class="text-center align-middle"><?= $pro['status'] ? 'Đang kinh doanh' : 'Ngừng kinh doanh' ?></td>
-                                <td class="text-center align-middle"><?= $pro['cate_name'] ?></td>
+                                
                                 <td class="text-center align-middle">
                                     <a href="<?= ADMIN_URL . '?ctl=editsp&id=' . $pro['id'] ?>">
                                         <button type="button" class="btn btn-outline-primary">
