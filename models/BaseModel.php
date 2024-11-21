931a68a6976595e587a33223110d32928b21edbf
tranhau02 <tranhau201202@gmail.com>
2024-11-21 23:36:03 +0700

    data

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -7,7 +7,7 @@ class BaseModel
     public function __construct()
     {
         try {
-            $this->conn = new PDO("mysql:host=localhost; dbname=da1-nhom2; charset=utf8; port=3306", "root", "");
+            $this->conn = new PDO("mysql:host=localhost; dbname=du-an-1; charset=utf8; port=3306", "root", "");
         } catch (PDOException $e) {
             echo "Lỗi kết nối CSDL: " . $e->getMessage();
         }
