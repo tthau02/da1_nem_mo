21caa25bb5f7e23fb38bc742151009e0b50d73d9
MiHii <hieupham08111@gmail.com>
2024-11-22 12:49:59 +0700

    fix login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const errorAlert = document.querySelector('.alert.alert-danger');
-      const authModal = document.getElementById('authModal');
+  const errorAlert = document.querySelector('.error-login');
+  const authModal = document.getElementById('authModal');
 
-      if (errorAlert && authModal) {
-          const modalInstance = bootstrap.Modal.getOrCreateInstance(authModal);
-          modalInstance.show();
-      }
+  // Chỉ hiển thị modal khi có lỗi đăng nhập
+  if (errorAlert && authModal) {
+    const modalInstance = bootstrap.Modal.getOrCreateInstance(authModal);
+    modalInstance.show();
+  }
 });
